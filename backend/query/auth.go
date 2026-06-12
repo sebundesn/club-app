6e94ecf097df5073d17fbbacdcd7932e88053305
sebundesn <sebundesn@icloud.com>
2026-06-12 16:49:28 +0900

    fix: ドキュメント変更

--- backend/query/auth.go
+++ backend/query/auth.go
@@ -4,7 +4,8 @@ const UserTable = `
 	CREATE TABLE IF NOT EXISTS users  (
 		id SERIAL PRIMARY KEY,
 		student_id VARCHAR(7) UNIQUE NOT NULL,
-		name TEXT NOT NULL,
+		username TEXT DEFAULT NULL,
+		realname TEXT DEFAULT NULL,
 		role TEXT,
 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	);
--- backend/query/managementQuery.go
+++ backend/query/managementQuery.go
@@ -17,6 +17,7 @@ const SelectMembers = `
 	SELECT student_id, name, role FROM users;
 `
 const NameChangeFirst = `
-	UPDATE users VALUES(realname, username)
-	SET ($2, $3) WHERE id = $1;
+	UPDATE users
+	SET realname = $2, username = $3
+	WHERE id = $1;
 `
